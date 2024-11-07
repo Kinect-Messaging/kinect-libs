82ecf62a872107d09522a1495ffaf142c005909f
Raj Palanisamy <>
2024-11-07 16:31:06 -0500

    Include constants for cloud events headers

--- common-utils/src/main/kotlin/com/kinectmessaging/libs/common/Constants.kt
+++ common-utils/src/main/kotlin/com/kinectmessaging/libs/common/Constants.kt
@@ -2,7 +2,8 @@ package com.kinectmessaging.libs.common
 
 object ErrorConstants {
     const val NO_DATA_FOUND_MESSAGE = "No valid data found for the provided input(s): "
-    const val HTTP_5XX_ERROR_MESSAGE = "System error while processing request. Check the error message for more details: "
+    const val HTTP_5XX_ERROR_MESSAGE =
+        "System error while processing request. Check the error message for more details: "
     const val HTTP_4XX_ERROR_MESSAGE = "Error while processing request. Check the error message for more details: "
 }
 
@@ -12,9 +13,18 @@ object LogConstants {
     const val SERVICE_DEBUG = "Service Debug "
 }
 
-object Defaults{
+object Defaults {
     const val PAGE_NO = 0
     const val PAGE_SIZE = 20
     const val TRANSACTION_ID_HEADER = "X-Transaction-Id"
     const val REQUEST_TIMEOUT = 60
+}
+
+object CloudEventsHeaders {
+    const val ID = "ce-id"
+    const val SPEC_VERSION = "ce-specversion"
+    const val TYPE = "ce-type"
+    const val SOURCE = "ce-source"
+    const val TIME = "ce-time"
+    const val SUBJECT = "ce-subject"
 }
--- common-utils/src/main/kotlin/com/kinectmessaging/libs/model/CloudEventsSchema.kt
+++ common-utils/src/main/kotlin/com/kinectmessaging/libs/model/CloudEventsSchema.kt
@@ -1,9 +1,11 @@
 package com.kinectmessaging.libs.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.JsonNode
 import java.time.LocalDateTime
 
+@JsonIgnoreProperties
 data class CloudEventsSchema(
     @JsonProperty("specversion")
     val specVersion: String,
